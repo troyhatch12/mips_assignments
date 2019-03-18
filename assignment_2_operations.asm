4e842644d87942abdc3629ff5ac9eb17d7368bf9
troyhatch12 <troyhatchard@gmail.com>
2019-03-17 21:08:11 -0600

    Initial Commit

---
+++ assignment_2_operations.asm
@@ -0,0 +1,16 @@
+#program to allow a user to input two integers to get
+#the result of adding, subtracting, multiplying, and 
+#dividing the two integers
+
+
+.text
+main:
+
+#input the first elements
+la $a0, 
+
+
+
+.data
+prompt_first_integer: .asciiz "Please input the first integer: \n:"
+prompt_second_integer: .asciiz "Please input the second integer: \n:"
